ccc9e44b22c1b9be84b221313fe9e008d77b8e42
siddharthpropelius <siddharth.vaishnav@propelius.tech>
2023-01-20 23:17:33 +0530

    threejs

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Hero from './components/Hero'
+import Details from './components/Details'
+
+const App = () => {
+  return (
+    <div>
+  <Hero/>    
+  <Details/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Details.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Details = () => {
+  return (
+    <div style={{height:"100vh"}}>
+      hello
+    </div>
+  )
+}
+
+export default Details
